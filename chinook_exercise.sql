cb32ee074b59e9ca23c939c34874e36922300462
juraj_kapasny <juraj@knoyd.com>
2021-10-31 07:23:46 +0100

    fixing link to chinook.db

--- chinook_exercise.sql
+++ chinook_exercise.sql
@@ -1,7 +1,7 @@
 /* SQL Exercise
 ====================================================================
 We will be working with database chinook.db
-You can download it here: https://drive.google.com/file/d/0Bz9_0VdXvv9bWUtqM0NBYzhKZ3c/view?usp=sharing
+You can download it here: https://drive.google.com/file/d/0Bz9_0VdXvv9bWUtqM0NBYzhKZ3c/view?usp=sharing&resourcekey=0-7zGUhDz0APEfX58SA8UKog
 
  The Chinook Database is about an imaginary video and music store. Each track is stored using one of the digital formats and has a genre. The store has also some playlists, where a single track can be part of several playlists. Orders are recorded for customers, but are called invoices. Every customer is assigned a support employee, and Employees report to other employees.
 */
